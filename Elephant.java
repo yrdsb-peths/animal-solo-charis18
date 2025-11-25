3d45d006eaa7de09b6040fbde8641eb7d24dcf1b
Charis <charis.tyc0108@gmail.com>
2025-11-25 17:47:41 -0500

    Add SimpleTimer class, add  into Elephant animation

--- Elephant.java
+++ Elephant.java
@@ -4,6 +4,25 @@ import greenfoot.GreenfootSound;  // (World, Actor, GreenfootImage, Greenfoot an
 public class Elephant extends Actor
 {
     private GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    private GreenfootImage[] idleRight = new GreenfootImage[8];
+    private GreenfootImage[] idleLeft = new GreenfootImage[8];
+    private int imageIndex = 0;
+    private String facing = "right";
+    private SimpleTimer animationTimer = new SimpleTimer();
+
+    public Elephant()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            idleRight[i] = new GreenfootImage("elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+            idleLeft[i] = new GreenfootImage(idleRight[i]);
+            idleLeft[i].mirrorHorizontally();
+        }
+        setImage(idleRight[0]);
+        animationTimer.mark();
+    }
+
     public void act()
     {
         if (Greenfoot.isKeyDown("left"))
@@ -29,6 +48,25 @@ public class Elephant extends Actor
             world.createApple();
             elephantSound.play();
         }    
+    }
+
+    public void animateElephant()
+    {
+        if (animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+
+        if (facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+        }
+        imageIndex = (imageIndex + 1) % idleRight.length;     
     }
 
 
